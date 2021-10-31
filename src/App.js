fba135b065cd866987484911714cf8d582d5e90e
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-10-31 15:09:49 -0700

    Character Count Added Character Count for jsx interpolation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import "./styles.css";
 
-const Message = (props) => <div className="message">{props.msg}</div>;
+function SayHello({ firstName, lastName }) {
+  return (
+    <div>
+      Hello {firstName} {lastName}
+    </div>
+  );
+}
+
+function CharacterCount({ text }) {
+  return (
+    <div className="container">
+      {`Text:  `}
+      <i>{` "${text}"`}</i>
+      {" has "}
+      {text.length ? <strong>{text.length}</strong> : "No"}
+      {" characters"}
+    </div>
+  );
+}
+
+const element = (
+  <>
+    <CharacterCount text={"Nate is here"} />
+    <CharacterCount text={"Jason went home"} />
+    <CharacterCount text={"Never plug that in"} />
+  </>
+);
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandboxes</h1>
-      <Message msg="Good Morning" />
-      <Message msg="Please sign in" />
+      <SayHello firstName={"donald"} lastName={"Kaiser"} />
+      <h2>{element}</h2>
     </div>
   );
 }
