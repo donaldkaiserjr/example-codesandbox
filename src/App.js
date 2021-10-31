b681291cd4b4a55a3a6c3ddb4dae4c1524b55f1a
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-10-31 13:24:37 -0700

    Message Prop Added test message prop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./styles.css";
 
-const element = document.createElement("div");
-element.textContent = "Hello World";
-element.className = "container";
+const Message = (props) => <div className="message">{props.msg}</div>;
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandboxes</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Message msg="Good Morning" />
+      <Message msg="Please sign in" />
     </div>
   );
 }
