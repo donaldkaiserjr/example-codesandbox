8d229960cb30026f225cadfc3d473889d2881257
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-10-31 21:08:20 -0700

    Boxes with Styling Added greeting boxes with some styling

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function SayHello({ firstName, lastName }) {
 function CharacterCount({ text }) {
   return (
     <div className="charCount">
-      {`Text:  `}
+      {`The sentence:  `}
       <i>{` "${text}"`}</i>
       {" has "}
       {text.length ? <strong>{text.length}</strong> : "No"}
@@ -20,19 +20,49 @@ function CharacterCount({ text }) {
   );
 }
 
+function Box({ className = "", style, size, ...rest }) {
+  const sizeClassName = size ? `box--${size}` : "box--large";
+
+  const props = {
+    className: `box ${sizeClassName}`,
+    style: { fontStyle: "normal", fontWeight: "600", color: "#fff", ...style },
+    ...rest
+  };
+  return <div {...props} />;
+}
+
 const element = (
   <>
     <CharacterCount text={"Nate is here"} />
     <CharacterCount text={"Jason went home"} />
     <CharacterCount text={"Never plug that in"} />
+    <Box
+      size="large"
+      style={{ backgroundColor: "purple" }}
+      className="container"
+    >
+      {" "}
+      Good Evening
+    </Box>
+    <Box style={{ backgroundColor: "skyblue" }} size="large">
+      {" "}
+      Good Night There Handsome{" "}
+    </Box>
+    <Box style={{ backgroundColor: "skyblue" }} size="medium">
+      {" "}
+      Good Night There Handsome{" "}
+    </Box>
+    <Box style={{ backgroundColor: "skyblue" }} size="small">
+      {" "}
+      Good Night There Handsome{" "}
+    </Box>
   </>
 );
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandboxes</h1>
-      <SayHello firstName={"donald"} lastName={"Kaiser"} />
+      <SayHello firstName={"Anthony"} lastName={"Phillips"} />
       <h2>{element}</h2>
     </div>
   );
