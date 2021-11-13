9539f067a8d63e211141cc9f29db146b4aab2cef
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-11-12 16:04:26 -0800

    test test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import AppAPI from "./APIs/AppAPI";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
+    <AppAPI />
     <App />
   </StrictMode>,
   rootElement
