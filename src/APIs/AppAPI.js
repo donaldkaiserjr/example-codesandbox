b18c836aaa08714bac0380debdbc72e5a15c9535
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-11-12 17:47:10 -0800

    AppAPI update Erased unneeded comments

---
+++ src/APIs/AppAPI.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class AppAPI extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      character: {}
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    fetch("https://swapi.dev/api/people/1")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          loading: false,
+          character: data
+        });
+      });
+  }
+
+  render() {
+    const text = this.state.loading ? "loading..." : this.state.character.name;
+    return <div>{text}</div>;
+  }
+}
+
+export default AppAPI;
