a06db4b78152d4a4ad5d9c2a828b218e20ff28c4
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-11-12 16:04:57 -0800

    initial commit

---
+++ src/APIs/AppAPI.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AppAPI extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <div>Code goes in here.</div>;
+  }
+}
+
+export default AppAPI;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,11 @@ import React from "react";
 function TodoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.item.completed} />
+      <input
+        type="checkbox"
+        onChange={() => console.log("Changed!")}
+        checked={props.item.completed}
+      />
       <div>{props.item.text}</div>
       <hr />
     </div>
