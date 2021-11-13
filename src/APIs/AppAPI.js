e5a54407816083916da36fd3ba03b3ffe331577c
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-11-12 17:45:00 -0800

    Update AppAPI.js

--- src/APIs/AppAPI.js
+++ src/APIs/AppAPI.js
@@ -3,10 +3,51 @@ import React, { Component } from "react";
 class AppAPI extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      loading: false,
+      character: {}
+    };
   }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    fetch("https://swapi.dev/api/people/1")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          loading: false,
+          character: data
+        });
+      });
+  }
+
+  // class App2 extends Component {
+  //   constructor() {
+  //     super();
+  //     this.state = {
+  //       isLoading: true
+  //     };
+  //   }
+
+  //   componentDidMount() {
+  //     setTimeout(() => {
+  //       this.setState({
+  //         isLoading: false
+  //       });
+  //     }, 1500);
+  //   }
+
+  //   render() {
+  //     return (
+  //       <div>{this.state.isLoading ? <h1>Loading...</h1> : <Conditional />}</div>
+  //     );
+  //   }
+  // }
+
   render() {
-    return <div>Code goes in here.</div>;
+    const text = this.state.loading ? "loading..." : this.state.character.name;
+    return <div>{text}</div>;
   }
 }
 
